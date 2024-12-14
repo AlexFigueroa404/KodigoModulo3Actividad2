6fe4154174ec7d06b76af64f174e3821c8c8b519
Figuer@ <alex.fuge503@gmail.com>
2024-12-14 12:15:44 -0600

    add findAllUsers method to controller and implement on service

--- src/main/java/com/kodigo/springboot/controller/StudentController.java
+++ src/main/java/com/kodigo/springboot/controller/StudentController.java
@@ -1,5 +1,33 @@
 package com.kodigo.springboot.controller;
 
+import com.kodigo.springboot.service.IStudentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/students")
 public class StudentController {
+
+  private final IStudentService studentService;
+
+
+  public StudentController(IStudentService studentService) {
+    this.studentService = studentService;
+  }
+
+
+  @GetMapping
+  public ResponseEntity<?> findAllStudents(){
+
+    return ResponseEntity.ok(studentService.findAllStudents());
+
+  }
+
+
+
+
 
 }
--- src/main/java/com/kodigo/springboot/service/StudentServiceImpl.java
+++ src/main/java/com/kodigo/springboot/service/StudentServiceImpl.java
@@ -1,5 +1,44 @@
 package com.kodigo.springboot.service;
 
-public class StudentServiceImpl {
+import com.kodigo.springboot.entity.Student;
+import com.kodigo.springboot.repository.StudentRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
 
+
+@Service
+public class StudentServiceImpl implements IStudentService {
+
+
+  private final StudentRepository studentRepository;
+
+  public StudentServiceImpl(StudentRepository studentRepository) {
+    this.studentRepository = studentRepository;
+  }
+
+  @Override
+  public List<Student> findAllStudents() {
+    return studentRepository.findAll();
+  }
+
+  @Override
+  public Optional<Student> findStudentById(Integer id) {
+    return Optional.empty();
+  }
+
+  @Override
+  public Student saveStudent(Student student) {
+    return null;
+  }
+
+  @Override
+  public Student updateStudent(Student student) {
+    return null;
+  }
+
+  @Override
+  public void deleteStudent(Integer id) {
+
+  }
 }
